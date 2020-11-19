15468b4a9d2df6efdc2d635a5f6b7b71fb09a4cf
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-11-19 02:13:15 -0300

    global style

---
+++ src/assets/styles/GlobalStyle.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export default createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #root {
+
+  height: 100vh;
+
+  font-family: 'PT Sans Caption', sans-serif;
+  
+}
+
+.App {
+
+  background: linear-gradient(-360deg, #41a9dc, #1588c7);
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+a {
+  cursor: pointer;
+}
+
+`;
+
