d61631392a4d1afd63b78bf3811cc996e8f95574
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-11-19 13:28:52 -0300

    toast alert

--- src/pages/LandingPage/components/Choice/index.tsx
+++ src/pages/LandingPage/components/Choice/index.tsx
@@ -3,13 +3,25 @@ import { Container, Options, Option, ImageContainer } from './styles'
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import leader from '../../../../assets/images/leader.png';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 
 export default function Choice() {
+
+  toast.configure();
 
   function scrollToLeadership() {
     const elmnt = document.getElementById('leadership');
     elmnt?.scrollIntoView({ behavior: 'smooth' });
+  }
 
+  function tryItAgain() {
+    window.scrollTo({ top: 0, behavior: 'smooth'})
+
+    toast.error('Sério? Que tal tentarmos de novo?' , {
+      autoClose: 2000
+    })
   }
 
   return (
@@ -17,7 +29,7 @@ export default function Choice() {
       
       <h1>Qual caminho você prefere?</h1>
       <Options>
-        <Option className='boss' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth'})}>
+        <Option className='boss' onClick={tryItAgain}>
           <ThumbDownIcon/>
           <h3>CHEFE</h3>
           <ul>
